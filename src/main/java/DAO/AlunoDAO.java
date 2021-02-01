997728dd7ade914899581c21be6d15a3f846fdd9
Davi Ferreira <daviferreirart@gmail.com>
2021-02-01 20:43:52 -0300

    Mudança na identação

--- src/main/java/DAO/AlunoDAO.java
+++ src/main/java/DAO/AlunoDAO.java
@@ -14,119 +14,113 @@ import Models.Aluno;
  */
 public abstract class AlunoDAO {
 
-    public static Aluno create(String nome, String sexo) {
-        Connection connection = Database.getConnection();
+	public static Aluno create(String nome, String sexo) {
+		Connection connection = Database.getConnection();
 
-        try {
-            nome = nome.trim().toUpperCase();
-            sexo = sexo.trim().toUpperCase();
-            PreparedStatement ps = connection.prepareStatement(
-                    "INSERT INTO ALUNOS(NOME,SEXO) VALUE(?,?);",
-                    PreparedStatement.RETURN_GENERATED_KEYS
-            );
-            ps.setString(1, nome);
-            ps.setString(2, sexo);
+		try {
+			nome = nome.trim().toUpperCase();
+			sexo = sexo.trim().toUpperCase();
+			PreparedStatement ps = connection.prepareStatement("INSERT INTO ALUNOS(NOME,SEXO) VALUE(?,?);",
+					PreparedStatement.RETURN_GENERATED_KEYS);
+			ps.setString(1, nome);
+			ps.setString(2, sexo);
 
-            if (sexo.equals("M") || sexo.equals("F")) {
+			if (sexo.equals("M") || sexo.equals("F")) {
 
-                ps.execute();
+				ps.execute();
 
-                ResultSet rs = ps.getGeneratedKeys();
-                rs.next();
+				ResultSet rs = ps.getGeneratedKeys();
+				rs.next();
 
-                int matricula = rs.getInt(1);
-                return new Aluno(matricula, nome, sexo);
-            } else {
-                JOptionPane.showMessageDialog(null, "Sexo deve ser M ou F !");
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            System.out.println(ex.getMessage());
-        }
-        return null;
-    }
+				int matricula = rs.getInt(1);
+				return new Aluno(matricula, nome, sexo);
+			} else {
+				JOptionPane.showMessageDialog(null, "Sexo deve ser M ou F !");
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			System.out.println(ex.getMessage());
+		}
+		return null;
+	}
 
-    public static Aluno findById(int matricula) {
-        Connection connection = Database.getConnection();
-        try {
-            PreparedStatement ps = connection.prepareStatement(""
-                    + "SELECT * FROM ALUNOS WHERE MATRICULA = ?");
+	public static Aluno findById(int matricula) {
+		Connection connection = Database.getConnection();
+		try {
+			PreparedStatement ps = connection.prepareStatement("" + "SELECT * FROM ALUNOS WHERE MATRICULA = ?");
 
-            ps.setInt(1, matricula);
+			ps.setInt(1, matricula);
 
-            ps.execute();
-            ResultSet rs = ps.getResultSet();
-            if (rs.next()) {
-                String nome = rs.getString("nome");
-                String sexo = rs.getString("sexo");
-                return new Aluno(matricula, nome, sexo);
+			ps.execute();
+			ResultSet rs = ps.getResultSet();
+			if (rs.next()) {
+				String nome = rs.getString("nome");
+				String sexo = rs.getString("sexo");
+				return new Aluno(matricula, nome, sexo);
 
-            } else {
-                JOptionPane.showMessageDialog(null, ""
-                        + "Não foi encontrado nenhum aluno com esta matricula");
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            System.out.println(ex.getMessage());
-        }
-        return null;
+			} else {
+				JOptionPane.showMessageDialog(null, "" + "Não foi encontrado nenhum aluno com esta matricula");
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			System.out.println(ex.getMessage());
+		}
+		return null;
 
-    }
+	}
 
-    public static void removeById(int matricula) {
-        Connection connection = Database.getConnection();
-        try {
-            PreparedStatement ps = connection.prepareStatement("DELETE FROM ALUNOS"
-                    + " WHERE MATRICULA = ?");
-            ps.setInt(1, matricula);
+	public static void removeById(int matricula) {
+		Connection connection = Database.getConnection();
+		try {
+			PreparedStatement ps = connection.prepareStatement("DELETE FROM ALUNOS" + " WHERE MATRICULA = ?");
+			ps.setInt(1, matricula);
 
-            int exec = ps.executeUpdate();
-            if (exec > 0) {
-                JOptionPane.showMessageDialog(null, "O aluno foi removido!");
-            } else {
-                JOptionPane.showMessageDialog(null, "O aluno não foi encontrado!");
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            System.out.println(ex.getMessage());
-        }
-    }
+			int exec = ps.executeUpdate();
+			if (exec > 0) {
+				JOptionPane.showMessageDialog(null, "O aluno foi removido!");
+			} else {
+				JOptionPane.showMessageDialog(null, "O aluno não foi encontrado!");
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			System.out.println(ex.getMessage());
+		}
+	}
 
-    public static void update(int matricula, String nome, String sexo) {
-        Connection connection = Database.getConnection();
-        nome = nome.trim().toUpperCase();
-        sexo = sexo.trim().toUpperCase();
-        String sql = "UPDATE ALUNOS SET";
-        boolean isFirst = true;
+	public static void update(int matricula, String nome, String sexo) {
+		Connection connection = Database.getConnection();
+		nome = nome.trim().toUpperCase();
+		sexo = sexo.trim().toUpperCase();
+		String sql = "UPDATE ALUNOS SET";
+		boolean isFirst = true;
 
-        if (!nome.equals("")) {
-            isFirst = false;
-            sql += " NOME = '" + nome + "'";
-        }
-        if (!sexo.equals("")) {
-            if (!isFirst) {
-                sql += ",";
-            } else {
-                isFirst = false;
-            }
-            sql += " SEXO = '" + sexo + "'";
-        }
+		if (!nome.equals("")) {
+			isFirst = false;
+			sql += " NOME = '" + nome + "'";
+		}
+		if (!sexo.equals("")) {
+			if (!isFirst) {
+				sql += ",";
+			} else {
+				isFirst = false;
+			}
+			sql += " SEXO = '" + sexo + "'";
+		}
 
-        sql += " WHERE MATRICULA = " + matricula;
+		sql += " WHERE MATRICULA = " + matricula;
 
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql);
-            int exec = ps.executeUpdate();
-            if (exec > 0) {
-                JOptionPane.showMessageDialog(null, "Os dados foram alterados!");
-            } else {
-                JOptionPane.showMessageDialog(null, "Não existe nenhum aluno"
-                        + " com esta matricula");
-            }
+		try {
+			PreparedStatement ps = connection.prepareStatement(sql);
+			int exec = ps.executeUpdate();
+			if (exec > 0) {
+				JOptionPane.showMessageDialog(null, "Os dados foram alterados!");
+			} else {
+				JOptionPane.showMessageDialog(null, "Não existe nenhum aluno" + " com esta matricula");
+			}
 
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            System.out.println(ex.getMessage());
-        }
-    }
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			System.out.println(ex.getMessage());
+		}
+	}
 }
