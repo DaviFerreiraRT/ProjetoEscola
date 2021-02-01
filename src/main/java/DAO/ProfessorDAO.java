abd61b64c67b5ff523149dba1897cb20128cdaf9
Davi Ferreira <daviferreirart@gmail.com>
2021-02-01 20:46:31 -0300

    Mudança na identação

--- src/main/java/DAO/ProfessorDAO.java
+++ src/main/java/DAO/ProfessorDAO.java
@@ -14,112 +14,107 @@ import javax.swing.JOptionPane;
  */
 public class ProfessorDAO {
 
-    public static Professor create(String nome, String sexo) {
-        Connection connection = Database.getConnection();
-        nome = nome.trim().toUpperCase();
-        sexo = sexo.trim().toUpperCase();
-        try {
-            PreparedStatement ps = connection.prepareStatement(""
-                    + "INSERT INTO PROFESSOR(NOME,SEXO) VALUES (?,?);",
-                    PreparedStatement.RETURN_GENERATED_KEYS);
-            ps.setString(1, nome);
-            ps.setString(2, sexo);
-            if (sexo.equals("M") || sexo.equals("F")) {
-                ps.executeUpdate();
-                ResultSet rs = ps.getGeneratedKeys();
-                rs.next();
+	public static Professor create(String nome, String sexo) {
+		Connection connection = Database.getConnection();
+		nome = nome.trim().toUpperCase();
+		sexo = sexo.trim().toUpperCase();
+		try {
+			PreparedStatement ps = connection.prepareStatement("INSERT INTO PROFESSOR(NOME,SEXO) VALUES (?,?);",
+					PreparedStatement.RETURN_GENERATED_KEYS);
+			ps.setString(1, nome);
+			ps.setString(2, sexo);
+			if (sexo.equals("M") || sexo.equals("F")) {
+				ps.executeUpdate();
+				ResultSet rs = ps.getGeneratedKeys();
+				rs.next();
 
-                int id = rs.getInt(1);
-                return new Professor(id, nome, sexo);
-            } else {
-                JOptionPane.showMessageDialog(null, "Sexo deve ser M ou F!");
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            System.out.println(ex.getMessage());
-        }
-        return null;
-    }
+				int id = rs.getInt(1);
+				return new Professor(id, nome, sexo);
+			} else {
+				JOptionPane.showMessageDialog(null, "Sexo deve ser M ou F!");
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			System.out.println(ex.getMessage());
+		}
+		return null;
+	}
 
-    public static Professor findById(int id) {
-        Connection connection = Database.getConnection();
-        try {
-            PreparedStatement ps = connection.prepareStatement(""
-                    + "SELECT * FROM PROFESSOR WHERE ID = ?");
+	public static Professor findById(int id) {
+		Connection connection = Database.getConnection();
+		try {
+			PreparedStatement ps = connection.prepareStatement("SELECT * FROM PROFESSOR WHERE ID = ?");
 
-            ps.setInt(1, id);
+			ps.setInt(1, id);
 
-            ps.execute();
-            ResultSet rs = ps.getResultSet();
-            if (rs.next()) {
-                String nome = rs.getString("nome");
-                String sexo = rs.getString("sexo");
-                return new Professor(id, nome, sexo);
+			ps.execute();
+			ResultSet rs = ps.getResultSet();
+			if (rs.next()) {
+				String nome = rs.getString("nome");
+				String sexo = rs.getString("sexo");
+				return new Professor(id, nome, sexo);
 
-            } else {
-                JOptionPane.showMessageDialog(null, ""
-                        + "Não foi encontrado nenhum professor com este ID!");
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            System.out.println(ex.getMessage());
-        }
-        return null;
-    }
+			} else {
+				JOptionPane.showMessageDialog(null,"Não foi encontrado nenhum professor com este ID!");
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			System.out.println(ex.getMessage());
+		}
+		return null;
+	}
 
-    public static void removeById(int id) {
-        Connection connection = Database.getConnection();
-        try {
-            PreparedStatement ps = connection.prepareStatement(""
-                    + "DELETE FROM PROFESSOR WHERE ID=?");
-            ps.setInt(1, id);
-            int exe=ps.executeUpdate();
-            if(exe>0){
-                JOptionPane.showMessageDialog(null, "O professor foi removido");
-            }else{
-                JOptionPane.showMessageDialog(null,""
-                        + "O professor com este ID não foi encontrado");
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            System.out.println(ex.getMessage());
-        }
-    }
-    public static void update(int id, String nome, String sexo) {
-        Connection connection = Database.getConnection();
-        nome = nome.trim().toUpperCase();
-        sexo = sexo.trim().toUpperCase();
-        String sql = "UPDATE PROFESSOR SET";
-        boolean isFirst = true;
+	public static void removeById(int id) {
+		Connection connection = Database.getConnection();
+		try {
+			PreparedStatement ps = connection.prepareStatement("DELETE FROM PROFESSOR WHERE ID=?");
+			ps.setInt(1, id);
+			int exe = ps.executeUpdate();
+			if (exe > 0) {
+				JOptionPane.showMessageDialog(null, "O professor foi removido");
+			} else {
+				JOptionPane.showMessageDialog(null,"O professor com este ID não foi encontrado");
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			System.out.println(ex.getMessage());
+		}
+	}
 
-        if (!nome.equals("")) {
-            isFirst = false;
-            sql += " NOME = '" + nome + "'";
-        }
-        if (!sexo.equals("")) {
-            if (!isFirst) {
-                sql += ",";
-            } else {
-                isFirst = false;
-            }
-            sql += " SEXO = '" + sexo + "'";
-        }
+	public static void update(int id, String nome, String sexo) {
+		Connection connection = Database.getConnection();
+		nome = nome.trim().toUpperCase();
+		sexo = sexo.trim().toUpperCase();
+		String sql = "UPDATE PROFESSOR SET";
+		boolean isFirst = true;
 
-        sql += " WHERE ID = " + id;
+		if (!nome.equals("")) {
+			isFirst = false;
+			sql += " NOME = '" + nome + "'";
+		}
+		if (!sexo.equals("")) {
+			if (!isFirst) {
+				sql += ",";
+			} else {
+				isFirst = false;
+			}
+			sql += " SEXO = '" + sexo + "'";
+		}
 
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql);
-            int exec = ps.executeUpdate();
-            if (exec > 0) {
-                JOptionPane.showMessageDialog(null, "Os dados foram alterados!");
-            } else {
-                JOptionPane.showMessageDialog(null, "Não existe nenhum professor"
-                        + " com este ID");
-            }
+		sql += " WHERE ID = " + id;
 
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            System.out.println(ex.getMessage());
-        }
-    }
+		try {
+			PreparedStatement ps = connection.prepareStatement(sql);
+			int exec = ps.executeUpdate();
+			if (exec > 0) {
+				JOptionPane.showMessageDialog(null, "Os dados foram alterados!");
+			} else {
+				JOptionPane.showMessageDialog(null, "Não existe nenhum professor" + " com este ID");
+			}
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			System.out.println(ex.getMessage());
+		}
+	}
 }
