46ff31ae627fe2e81d405c82cb3d0946070607f0
Davi Ferreira <daviferreirart@gmail.com>
2021-02-01 21:49:07 -0300

    Mudança no método create

--- src/main/java/DAO/TurmaDAO.java
+++ src/main/java/DAO/TurmaDAO.java
@@ -2,8 +2,11 @@ package DAO;
 
 import Models.Turma;
 import Database.Database;
+import Models.Professor;
+import Models.Disciplina;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -12,19 +15,27 @@ import java.sql.SQLException;
  */
 public class TurmaDAO {
 
-    public static Turma create(String nome, int professorId, int disciplinaId) {
-        Connection connection = Database.getConnection();
-        try {
-            PreparedStatement ps = connection.prepareStatement(""
-                    + "INSERT INTO TURMA(NOME,PROFESSORID,DISCIPLINAID VALUES"
-                    + "(?,?,?)");
-            ps.setString(1, nome);
-            ps.setInt(2, professorId);
-            ps.setInt(3, disciplinaId);
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            System.out.println(ex.getMessage());
-        }
-        return null;
-    }
+	public static Turma create(String nome, int professorId, int disciplinaId) {
+		Connection connection = Database.getConnection();
+		nome = nome.trim().toUpperCase();
+		Professor professor = ProfessorDAO.findById(professorId);
+		Disciplina disciplina = DisciplinaDAO.findById(disciplinaId);
+		try {
+			PreparedStatement ps = connection.prepareStatement(
+					"INSERT INTO TURMA(NOME,PROFESSORID,DISCIPLINAID) VALUES (?,?,?)",
+					PreparedStatement.RETURN_GENERATED_KEYS);
+			ps.setString(1, nome);
+			ps.setInt(2, professorId);
+			ps.setInt(3, disciplinaId);
+			ps.executeUpdate();
+			ResultSet rs = ps.getGeneratedKeys();
+			rs.next();
+			int id = rs.getInt(1);
+			return new Turma(id, nome, professor, disciplina);
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+			System.out.println(ex.getMessage());
+		}
+		return null;
+	}
 }
